42b48afc872327ba787d1974d22f618b4c01d50c
Anton Johansson <antoon.johansson@gmail.com>
2018-05-12 16:35:03 +0200

    Store graphs in an existing directory...

--- home-assistant/bash/download-graph.sh
+++ home-assistant/bash/download-graph.sh
@@ -16,4 +16,4 @@ dashboard=$2
 panel=$3
 file=$4
 
-curl -s -H "Authorization: Bearer $key" "http://grafana:3000/render/d-solo/Uvy4M-mmk/$dashboard?orgId=1&panelId=$panel&width=800&height=400&tz=UTC%2B02%3A00" > /home/.homeassistant/downloads/$file
+curl -s -H "Authorization: Bearer $key" "http://grafana:3000/render/d-solo/Uvy4M-mmk/$dashboard?orgId=1&panelId=$panel&width=800&height=400&tz=UTC%2B02%3A00" > /config/downloads/$file
