7f23cc6f9cc87fd275ec1a73489cecf3531bb58d
Anton Johansson <antoon.johansson@gmail.com>
2018-05-13 14:44:34 +0200

    Connect to WiFi and send sensor value to Home Assistant

--- arduino/soil_moisture_sensor/README.md
+++ arduino/soil_moisture_sensor/README.md
@@ -9,3 +9,10 @@ Provides a unit that checks the moisture in the soil for plants. Useful for remi
 | --------- | ----- | -------- |
 | [Wemos D1 mini](https://www.m.nu/esp8266/d1-mini-v30) | ![Wemos D1 mini](https://images.m.nu/data/product/1076f860/wemos_d1_mini_v30_1.jpg) | The Arduino board itself. |
 | [Soil moisture sensor](https://www.kjell.com/se/sortiment/el-verktyg/arduino/moduler/luxorparts-jordfuktsmatare-for-arduino-p87941) | ![Soil moisture sensor](https://www.kjell.com/se/image/Product_242997sv/full/1/luxorparts-jordfuktsmatare-for-arduino) | The sensor component. |
+
+
+## Code notes
+
+### Enforcing data to be sent over HTTP
+
+When sending data over HTTP, the only way to enforce it to actually be sent is to read something from the response. Reading the entire response body (`client.readString()`) seems to be an expensive operation, and takes a few seconds to perform. Therefore, it's useful to read to the first line separator (`client.readStringUntil('\n');`). This also provides useful information, so we easily can see whether or not the call succeeded (`200 OK`).
--- arduino/soil_moisture_sensor/soil_moisture_sensor.ino
+++ arduino/soil_moisture_sensor/soil_moisture_sensor.ino
@@ -1,25 +1,87 @@
-int MAXIMUM_VALUE = 1024;
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+
+// Entity-wide constants
+const char* WIFI_SSID = "...";
+const char* WIFI_PASSWORD = "...";
+const char* HOME_ASSISTANT_SENSOR_ID = "...";
+const char* HOME_ASSISTANT_DOMAIN = "...";
+const char* HOME_ASSISTANT_PASSWORD = "...";
+
+// Script-wide constants
+const int MAXIMUM_VALUE = 1024;
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Initializing sensor");
   pinMode(BUILTIN_LED, OUTPUT);
+  digitalWrite(BUILTIN_LED, HIGH);
+
+  connectToWiFi();
 }
 
 void loop() {
+  delay(5000);
+  digitalWrite(BUILTIN_LED, LOW);
+
   int sensorValue = MAXIMUM_VALUE - analogRead(A0);
   output(sensorValue);
-  delay(1000);
+  sendValueToHomeAssistant(sensorValue);
+
   digitalWrite(BUILTIN_LED, HIGH);
-  delay(1000);
-  digitalWrite(BUILTIN_LED, LOW);
+}
+
+void connectToWiFi() {
+  Serial.print("Connecting to Wi-Fi");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("Wi-Fi connected successfully");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("Signal strength: ");
+  Serial.print(WiFi.RSSI());
+  Serial.println(" dBm");
 }
 
 void output(int sensorValue) {
-  String output = "Value is ";
-  output = output + sensorValue;
-  output = output + " / ";
-  output = output + MAXIMUM_VALUE;
-  Serial.println(output);
+  Serial.print("Value: ");
+  Serial.print(sensorValue);
+  Serial.print("/");
+  Serial.println(MAXIMUM_VALUE);
+}
+
+void sendValueToHomeAssistant(int sensorValue) {
+  WiFiClientSecure client;
+  if (!client.connect(HOME_ASSISTANT_DOMAIN, 443)) {
+    Serial.print("Could not connect to https://");
+    Serial.println(HOME_ASSISTANT_DOMAIN);
+    return;
+  }
+
+  String body = String("{\"state\":") + sensorValue + "}";
+
+  client.print("POST /api/states/sensor.");
+  client.print(HOME_ASSISTANT_SENSOR_ID);
+  client.println(" HTTP/1.1");
+  client.print("Host: ");
+  client.println(HOME_ASSISTANT_DOMAIN);
+  client.print("X-HA-Access: ");
+  client.println(HOME_ASSISTANT_PASSWORD);
+  client.println("Content-Type: application/json");
+  client.print("Content-Length: ");
+  client.println(body.length());
+  client.println();
+  client.println(body);
+
+  // Read the first line, to enforce that data is sent
+  String line = client.readStringUntil('\n');
+  Serial.print("Value sent to Home Assistant, HTTP response: ");
+  Serial.println(line);
 }
 
