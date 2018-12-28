3538505ed7a0897ca290c47c40bfe9b579a5bf32
Anton Johansson <antoon.johansson@gmail.com>
2018-12-28 21:44:14 +0100

    Update the DNS records for all subdomains (not just for Home Assistant)
    Closes #42

--- home-assistant/scripts/update-dns-record.py
+++ home-assistant/scripts/update-dns-record.py
@@ -5,11 +5,10 @@ from sys import argv;
 
 # Constants
 endpoint = "https://api.loopia.se/RPCSERV";
-subdomain = "home";
 
 def main():
-    if len(argv) != 5:
-        print("Expected exactly 4 arguments");
+    if len(argv) < 6:
+        print("Expected atleast 5 arguments");
         return 1;
 
     # Input
@@ -17,39 +16,46 @@ def main():
     password = argv[2];
     domain = argv[3];
     address = argv[4];
+    subdomains = [];
+    for i in range(5, len(argv)):
+        subdomains.append(argv[i].split('.')[0]);
+
+    print("Checking subdomains: " + str(subdomains));
 
     # Create the XMLRPC client
     with xmlrpc.client.ServerProxy(endpoint) as client:
-        records = client.getZoneRecords(username, password, domain, subdomain);
-        print("Found records: " + str(records));
+        for subdomain in subdomains:
+            print("Checking subdomain: " + subdomain);
+            records = client.getZoneRecords(username, password, domain, subdomain);
+            print("Found records: " + str(records));
 
-        # Find an existing record
-        recordId = 0;
-        if len(records) > 0:
-            if records[0]["rdata"] == address:
-                print("No need to update the record, because it has the same value, exiting...");
-                return;
-            recordId = records[0]["record_id"];
+            # Find an existing record
+            recordId = 0;
+            if len(records) > 0:
+                if records[0]["rdata"] == address:
+                    print("No need to update the record, because it has the same value, exiting...");
+                    return;
+                recordId = records[0]["record_id"];
 
-        # Build the object
-        record = {
-            "type": "A",
-            "rdata": address,
-            "record_id": recordId,
-            "ttl": 3600,
-            "priority": 0
-        };
+            # Build the object
+            record = {
+                "type": "A",
+                "rdata": address,
+                "record_id": recordId,
+                "ttl": 3600,
+                "priority": 0
+            };
 
-        if recordId > 0:
-            # Update the existing record
-            print("Updating record " + str(recordId) + " for '" + subdomain + "." + domain + "' with address: " + address);
-            response = client.updateZoneRecord(username, password, domain, subdomain, record);
-            print("Response: " + str(response));
-        else:
-            # Create a new record
-            print("Creating a new record for '" + subdomain + "." + domain + "' with address: " + address);
-            response = client.addZoneRecord(username, password, domain, subdomain, record);
-            print("Response: " + str(response));
+            if recordId > 0:
+                # Update the existing record
+                print("Updating record " + str(recordId) + " for '" + subdomain + "." + domain + "' with address: " + address);
+                response = client.updateZoneRecord(username, password, domain, subdomain, record);
+                print("Response: " + str(response));
+            else:
+                # Create a new record
+                print("Creating a new record for '" + subdomain + "." + domain + "' with address: " + address);
+                response = client.addZoneRecord(username, password, domain, subdomain, record);
+                print("Response: " + str(response));
 
 # Run main method
 if __name__ == '__main__':
